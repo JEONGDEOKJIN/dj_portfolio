4283b2674fec41aa1052c5dd3a0f5bf48362a8f3
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2024-01-06 17:22:20 +0900

    0106 수정

--- src/contents/skills.jsx
+++ src/contents/skills.jsx
@@ -16,7 +16,7 @@ const Skills = () => {
                         언어
                     </h3>
                     <ul className="flex flex-col gap-[16px] text-[16px] pb-[32px] font-medium w-full items-start">
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
                                 <SiJavascript />
                                 <span className="md:w-[120px]">Javascript</span>
@@ -30,7 +30,7 @@ const Skills = () => {
                             </ul>
                         </li>
 
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
                                 <SiTypescript />
                                 <span className="md:w-[120px]">Typescript</span>
@@ -39,39 +39,10 @@ const Skills = () => {
                                 <li> interface 를 이용한 기본적인 type 설정 </li>
                             </ul>
                         </li>
+                    
                         
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
-                            <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
-                                <SiNextdotjs />
-                                <span className="md:w-[120px]">Next.js</span>
-                            </span>
-                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li> server component 와 client component 기능 구분 사용 </li>
-                                <li> useParams, searchParams 를 사용하여 동적 라우팅 사용 </li>
-                                <li> timebased 또는 on-demand 을 통한 revalidate 기능 사용</li>
-                            </ul>
-                        </li>
 
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
-                            <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
-                                <SiReactos />
-                                <span className="md:w-[120px]">React</span>
-                            </span>
-                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li> reactQuery, Mutation 활용한 캐싱 데이터 활용 </li>
-                            </ul>
-                        </li>
-                        
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
-                            <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
-                                <FaNodeJs />
-                                <span className="md:w-[120px]">Node.js</span>
-                            </span>
-                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li> sequelize, MySQL 활용하여 기본적인 CRUD 구현  </li>
-                            </ul>
-                        </li>
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start mt-2">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
                                 <SiSolidity />
                                 <span className="md:w-[120px]">Solidity</span>
@@ -81,27 +52,41 @@ const Skills = () => {
                             </ul>
                         </li>
                         
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+
+                        
+                    </ul>
+                </li>
+
+                <li className="flex flex-row gap-[32px] py-[32px]">
+                    <h3 className="text-[16px] md:text-[24px] font-extrabold md:font-semibold w-[20%] basisc-[25%]">
+                        FrontEnd
+                    </h3>
+                    <ul className="flex flex-col gap-[16px] text-[16px] pb-[32px] font-medium w-full items-start">
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
-                                <FaAws />
-                                <span className="md:w-[120px]"> AWS </span>
+                                <SiReact />
+                                <span className="md:w-[120px]">React</span>
                             </span>
                             <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li> EC2 활용하여 React 프로젝트 배포 경험 </li>
+                                <li>reactQuery, mutation 활용한 캐싱 데이터 활용 </li>
+                                <li>redux 활용한 상태 관리</li>
                             </ul>
                         </li>
-                        
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
-                                <FaGit />
-                                <span className="md:w-[120px]"> Git </span>
+                                <SiNextdotjs />
+                                <span className="md:w-[120px]">NextJS</span>
                             </span>
                             <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li> git 활용하여 버전관리 및 프로젝트 관리 </li>
+                                <li>Client Component 와 Servier component 최적화 사용</li>
+                                <li> useParams, searchParams 를 사용하여 동적 라우팅 사용 </li>
+                                <li> timebased 또는 on-demand 을 통한 revalidate 기능 사용</li>
+                                <li>web3 연결시 SSR 특징을 고려하여 연결 최적화</li>
                             </ul>
                         </li>
-                        
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
                                 <FaFigma />
                                 <span className="md:w-[120px]"> Figma </span>
@@ -116,41 +101,10 @@ const Skills = () => {
 
                 <li className="flex flex-row gap-[32px] py-[32px]">
                     <h3 className="text-[16px] md:text-[24px] font-extrabold md:font-semibold w-[20%] basisc-[25%]">
-                        FrontEnd
-                    </h3>
-                    <ul className="flex flex-col gap-[16px] text-[16px] pb-[32px] font-medium w-full items-start">
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
-                            <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
-                                <SiReact />
-                                <span className="md:w-[120px]">React</span>
-                            </span>
-                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li>CRA로 프로젝트 다수 생성 경험 보유!</li>
-                                <li>상태에 대해서 설명할수있음!</li>
-                                <li>브라우저 랜더링 과정을 설명 할 수 있음!</li>
-                            </ul>
-                        </li>
-
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
-                            <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
-                                <SiNextdotjs />
-                                <span className="md:w-[120px]">NextJS</span>
-                            </span>
-                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
-                                <li>Client Component 와 Servier component 최적화 사용</li>
-                                <li>클라이언트 컴포넌트에서는 useParams, 서버 컴포넌트에서는 searchParams 를 사용하여 동적 라우팅 사용</li>
-                                <li>web3 연결시 SSR 특징을 고려하여 연결 최적화</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-
-                <li className="flex flex-row gap-[32px] py-[32px]">
-                    <h3 className="text-[16px] md:text-[24px] font-extrabold md:font-semibold w-[20%] basisc-[25%]">
                         BackEnd
                     </h3>
                     <ul className="flex flex-col gap-[16px] text-[16px] pb-[32px] font-medium w-full items-start">
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
                                 <SiExpress />
                                 <span className="md:w-[120px]">Express</span>
@@ -159,6 +113,27 @@ const Skills = () => {
                                 <li>RESTful API 설계 및 CRUD 게시판 작성 경험</li>
                             </ul>
                         </li>
+
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
+                            <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
+                                <FaNodeJs />
+                                <span className="md:w-[120px]">Node.js</span>
+                            </span>
+                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
+                                <li> sequelize, MySQL 활용하여 기본적인 CRUD 구현  </li>
+                            </ul>
+                        </li>
+
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
+                            <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
+                                <FaAws />
+                                <span className="md:w-[120px]"> AWS </span>
+                            </span>
+                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
+                                <li> EC2 활용하여 React 프로젝트 배포 경험 </li>
+                            </ul>
+                        </li>
+
                     </ul>
                 </li>
 
@@ -167,7 +142,18 @@ const Skills = () => {
                         ETC
                     </h3>
                     <ul className="flex flex-col gap-[16px] text-[16px] pb-[32px] font-medium w-full items-start">
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
+                            <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
+                                <FaGit />
+                                <span className="md:w-[120px]"> Git </span>
+                            </span>
+                            <ul className="list-disc list-outside leading-6 px-[20px] md:leading-8 text-[12px] md:text-[16px] ">
+                                <li> git 활용하여 버전관리 및 프로젝트 관리 </li>
+                            </ul>
+                        </li>
+
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
                                 <SiUnrealengine />
                                 <span className="md:w-[120px]">Unreal</span>
@@ -176,7 +162,7 @@ const Skills = () => {
                                 <li>블루프린트 활용한 3D 애셋 제작 및 레벨 디자인</li>
                             </ul>
                         </li>
-                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-baseline">
+                        <li className="flex flex-col gap-[8px] md:gap-[32px] md:flex-row items-start">
                             <span className="text-[16px] text-slate-600 md:text-[24px] basis-[20%] flex items-center gap-[8px]">
                                 <DiPhotoshop />
                                 <span className="md:w-[120px]">Photoshop</span>
