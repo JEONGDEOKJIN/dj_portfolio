5330f1910d459eecbf8107c80e74a79fd6e98799
JEONGDEOKJIN <anotheryear.hm@gmail.com>
2024-01-24 11:06:01 +0900

    0124 포트폴리오 설명 추가

--- src/contents/STOproject/ModalDetail_2.jsx
+++ src/contents/STOproject/ModalDetail_2.jsx
@@ -100,13 +100,13 @@ const ModalDetail_2 = () => {
 
                     <h3 className="py-[16px] text-[16px] font-bold mt-6">업무 상세</h3>
                     <ul className="px-[32px] leading-8 list-disc list-outside text-[12px] md:text-[16px]">
-                        <li> [이슈] 협약사는 '소유(부동산 STO 회사)'를 레퍼런스로 요청하였으나, 해당 어플에는 '하단 메뉴바' 가 없었음. </li>
+                        <li> [이슈] 협약사는 '소유(부동산 STO 회사)'를 레퍼런스로 요청. 해당 어플에는 '하단 메뉴바' 가 없었음. </li>
                         <li> [해결과정]</li>
-                            <li className='ml-10 list-none '> 1. '메뉴 하단 바'가 없는 경우, 정보접근성이 떨어진다고 판단. </li>
-                            <li className='ml-20 text-sm list-none '> EX) 거래 페이지를 보다가, '물건 팔 때, 내가 청약한 물건을 얼마였지?' 를 참고 하고 싶다는 생각이 들면 '현재 페이지 - 홈 - 마이페이지' 로 '페이지 이동' 을 하게 됨 </li>
-                            <li className='ml-10 list-none '> 2. '투자' 는 복합적인 판단을 필요함으로 다양한 정보게 빠르게 접근할 수 있어야 함 </li>
+                            <li className='ml-10 list-none '> 1. '메뉴 하단 바'가 없는 경우, 정보접근성이 떨어진다고 판단</li>
+                            <li className='ml-20 text-sm text-gray-500 list-none'> EX) 거래 페이지를 보다가, '물건 팔 때, 내가 청약한 물건을 얼마였지?' 를 참고 하고 싶다는 생각이 들면 '현재 페이지 - 홈 - 마이페이지' 로 '페이지 이동' 을 하게 됨 </li>
+                            <li className='ml-10 list-none '> 2. 고객 관점에서 '투자' 는 복합적 판단이 필요하기에 다양한 정보에 빠른 접근이 필요 </li>
                             <li className='ml-10 list-none '> 3. ''메뉴 하단 바' 추가를 통해, 정보 접근의 효율성 제고 </li>
-                            <li className='ml-20 text-sm list-none '> EX) 거래 페이지를 보다가, '물건 팔 때, 내가 청약한 물건을 얼마였지?' 를 참고 하고 싶다는 생각이 들면 '페이지 전환 없이' 마이페이지로 접근 가능 </li>
+                            <li className='ml-20 text-sm text-gray-500 list-none'> EX) 거래 페이지를 보다가, '물건 팔 때, 내가 청약한 물건을 얼마였지?' 를 참고 하고 싶다는 생각이 들면 '페이지 전환 없이' 마이페이지로 접근 가능 </li>
 
                         
                         {/* <li> '블랙리스트 유저' 는 '협약사 요청사항'으로써, 메인페이지에서 확인할 수 있게 구현 </li>
